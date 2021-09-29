1e83d316fce7558956bdc13d1d7e1d5306be4b5e
Schitzor <61148167+unl0cks@users.noreply.github.com>
2021-09-29 19:38:55 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,10 +6,8 @@ from dotenv import load_dotenv
 import datetime
 
 # Setup main.env and run Discord.
-load_dotenv('main.env')
+load_dotenv()
 token = os.getenv('token')
-
-client = discord.Client()
 bot = commands.Bot(command_prefix='!')
 
 # Events for the bot to perform.
@@ -17,17 +15,13 @@ bot = commands.Bot(command_prefix='!')
 async def on_ready():
   # Print this when the bot starts up for the first time.
   print(f"{bot.user} has connected to Discord!")
-
-@bot.event
 async def on_message(message):
   # Ignore messages from the bot itself so that there's no conflict.
   if message.author == bot.user:
     return
-
   if message.content == 'thanks':
     # Respond to thanks.
     await message.channel.send("You're welcome!")
-  
   await bot.process_commands(message)
 
 # Bot commands.
@@ -36,20 +30,17 @@ async def ping(ctx):
   await ctx.send('pong')
 
 @bot.command()
+async def youtube(ctx):
+  await ctx.send('https://www.youtube.com/Schitzor')
+
+@bot.command()
 async def info(ctx):
   embed = discord.Embed(title=f"{ctx.guild.name}", description="Embed bot made using Python 3.9.7.", timestamp=datetime.datetime.utcnow(), color=discord.Color.blue())
   embed.add_field(name="Server created at", value=f"{ctx.guild.created_at}")
   embed.add_field(name="Server Owner", value=f"{ctx.guild.owner}")
   embed.add_field(name="Server Region", value=f"{ctx.guild.region}")
   embed.add_field(name="Server ID", value=f"{ctx.guild.id}")
-
   await ctx.send(embed=embed)
-
-@bot.listen()
-async def on_message(message):
-  if 'youtube' in message.content.lower():
-    await message.channel.send('https://www.youtube.com/Schitzor')
-    await bot.process_commands(message)
 
 
 bot.run(token)
