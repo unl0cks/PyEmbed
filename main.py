9505b6c5bea3fd3a7e8308b1c3d19e3ff08f6a2e
Schitzor <61148167+unl0cks@users.noreply.github.com>
2021-09-29 19:30:10 +0200

    hahahahaha

--- main.py
+++ main.py
@@ -2,44 +2,43 @@
 import os
 import discord
 from discord.ext import commands
-from discord.ext.commands import Bot
 from dotenv import load_dotenv
 import datetime
 
 # Setup main.env and run Discord.
 load_dotenv()
-token = os.getenv('token')
+gettoken = os.getenv('token')
 
-bot = commands.Bot(command_prefix='!')
+getbot1 = commands.Bot(command_prefix='!')
 
 # Events for the bot to perform.
-@bot.event
+@getbot1.event
 async def on_ready():
   # Print this when the bot starts up for the first time.
-  print(f"{bot.user} has connected to Discord!")
+  print(f"{getbot1.user} has connected to Discord!")
 
-@bot.event
+@getbot1.event
 async def on_message(message):
   # Ignore messages from the bot itself so that there's no conflict.
-  if message.author == bot.user:
+  if message.author == getbot1.user:
     return
 
   if message.content == 'thanks':
     # Respond to thanks.
     await message.channel.send("You're welcome!")
   
-  await bot.process_commands(message)
+  await getbot1.process_commands(message)
 
 # Bot commands.
-@bot.command()
+@getbot1.command()
 async def ping(ctx):
   await ctx.send('pong')
   
-@bot.command()
+@getbot1.command()
 async def youtube(ctx):
   await ctx.send('https://www.youtube.com/Schitzor')
 
-@bot.command()
+@getbot1.command()
 async def info(ctx):
   embed = discord.Embed(title=f"{ctx.guild.name}", description="Embed bot made using Python 3.9.7.", timestamp=datetime.datetime.utcnow(), color=discord.Color.blue())
   embed.add_field(name="Server created at", value=f"{ctx.guild.created_at}")
@@ -50,4 +49,4 @@ async def info(ctx):
   await ctx.send(embed=embed)
 
 
-bot.run(token)
+getbot.run(gettoken)
